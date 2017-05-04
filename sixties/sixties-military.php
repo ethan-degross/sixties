55e662c5a121861d2456767f72186e727664638d
Ethan DeGross <ethan-degross@uiowa.edu>
2017-05-04 10:15:42 -0500

    Revert "Added the first set of images from the ROTC Album"
    This reverts commit a3ddb8f99f9dad34e4dea15d355334c89bf84613.

--- sixties/sixties-military.php
+++ sixties/sixties-military.php
@@ -27,64 +27,7 @@ get_header(); ?>
 			Military Service
 			</div>
 			<p>
-				The UI Reserve Officers Training Corps (ROTC) during most of the decade consisted of a Brigade of Army Cadets and a Wing of Air Force Cadets. Look through the University Archives’ photo albums that chronicle ROTC’s activity: 1966-1967, 1967-1968, <a href="http://digital.lib.uiowa.edu/cdm/ref/collection/socjustice/id/2904">1968-1969</a>, 1969-1970, 1970-1971, or 1971-1972. (More content coming soon!)
-		</div>
-
-		<div id="single-rotc-album-container">
-		<div class="owl-carousel sixtiesImages">
-
-			<div class="single-rotc-album">
-				<a href="http://digital.lib.uiowa.edu/cdm/ref/collection/socjustice/id/2927">
-			<img src="https://dsps.lib.uiowa.edu/sixties/wp-content/uploads/sites/7/2017/04/rotcAlbumSixties600x600.jpg">
-			<p id="samtitle">Young ROTC Member Shining His Shoes</p>
-				</a>
-			</div>
-			<div class="single-rotc-album">
-				<a href="http://digital.lib.uiowa.edu/cdm/ref/collection/socjustice/id/2928">
-			<img src="https://dsps.lib.uiowa.edu/sixties/wp-content/uploads/sites/7/2017/04/rotcAlbum1-2.jpg">
-			<p id="samtitle">Practice for the Regimental Drill Meet at Nebraska</p>
-				</a>
-			</div>
-			<div class="single-rotc-album">
-				<a href="http://digital.lib.uiowa.edu/cdm/ref/collection/socjustice/id/2942">
-			<img src="https://dsps.lib.uiowa.edu/sixties/wp-content/uploads/sites/7/2017/04/Photo_60_3_600x600samsample.jpg">
-			<p id="samtitle">Image from the Regimental Drill Meet at Nebraska</p>
-				</a>
-			</div>
-			<div class="single-rotc-album">
-				<a href="http://digital.lib.uiowa.edu/cdm/ref/collection/socjustice/id/2898">
-			<img src="https://dsps.lib.uiowa.edu/sixties/wp-content/uploads/sites/7/2017/04/Photo_16_3_600x600samsample.jpg">
-			<p id="samtitle">Young Women Walk in the 1968 Guidons Ceromony</p><!--need more info-->
-				</a>
-			</div>
-			<div class="single-rotc-album">
-				<a href="http://digital.lib.uiowa.edu/cdm/ref/collection/socjustice/id/2908">
-			<img src="https://dsps.lib.uiowa.edu/sixties/wp-content/uploads/sites/7/2017/04/Photo_26_4_600x600vs.jpg">
-			<p id="samtitle">Couple Dancing at the Military Ball</p><!--need more info-->
-				</a>
-			</div>
-			<div class="single-rotc-album">
-				<a href="http://digital.lib.uiowa.edu/cdm/ref/collection/socjustice/id/2910">
-			<img src="https://dsps.lib.uiowa.edu/sixties/wp-content/uploads/sites/7/2017/04/Photo_28_1_600x600v2.jpg">
-			<p id="samtitle">Couples Stand Over the Crowd at the Military Ball</p><!--need more info-->
-				</a>
-			</div>
-			<div class="single-rotc-album">
-				<a href="http://digital.lib.uiowa.edu/cdm/ref/collection/socjustice/id/2891">
-			<img src="https://dsps.lib.uiowa.edu/sixties/wp-content/uploads/sites/7/2017/04/Photo9_1_600x600.jpg">
-			<p id="samtitle">ROTC Flag Carriers in the Homecoming Parade</p><!--need more info-->
-				</a>
-			</div>
-			<div class="single-rotc-album">
-				<a href="http://digital.lib.uiowa.edu/cdm/ref/collection/socjustice/id/2904">
-			<img src="https://dsps.lib.uiowa.edu/sixties/wp-content/uploads/sites/7/2017/04/Photo_22_4_600x600.jpg">
-			<p id="samtitle">ROTC Members at the Regimental Inspection</p><!--need more info-->
-				</a>
-			</div>
-		</div>
-
-		<div id="source">
-			<p id="sourcewords">Source: Army ROTC photograph album, 1968-1969</p>
+				The UI Reserve Officers Training Corps (ROTC) during most of the decade consisted of a Brigade of Army Cadets and a Wing of Air Force Cadets. Look through the University Archives’ photo albums that chronicle ROTC’s activity from 1966 to 1972 (coming soon).
 		</div>
 </div>
 <div id="coming-soon" align="center">
